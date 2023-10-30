b3ff70417d7fdcb4dd30f3082d7ff0f8fbe07dc8
Medineygt <147553787+Medineygt@users.noreply.github.com>
2023-10-31 00:05:14 +0300

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,70 @@
+use vtys
+select*from Adresler
+insert into adresler values('kaya','yasin',23,'izmir','35000','Türkiye'),
+('menekþe','polat',12,'konya','41000','türkiye'),
+('yeþil','pýnar',4,'ordu','24000','türkiye')
+
+select * from UYELER
+Insert into uyeler
+values('Medine','Yiðit','Kýz','052462358233','medyigit1641@gmail.com',15),
+('Hakan','Solmaz','Erkek','05465272635','hakocano@hotmail.com',4),
+('Öykü','Duman','Kýz','20212320134','oykudmn21@gmail.com',64)
+
+select * from Emanet
+insert into emanet values
+('2123',12,21,'2020-11-30 11:12:56','2021-10-30 16:18:50'),
+('34234',32,45,'2022-10-10 12:11:22','2022-10-30 04:32:38'),
+('1452',34,41,'2020-10-30 05:18:38','2021-10-30 15:20:38')
+
+
+select *from Kategoriler
+insert into Kategoriler values
+('edebiyat'),
+('doða bilimleri'),
+('tarih')
+
+select * from KitapKategori
+insert into KitapKategori values
+('felsefe',3),
+('din',6),
+('fen bilimleri',2)
+
+select * from KitapKutuphane
+insert into KitapKutuphane values
+(3,'324234',6),
+(9,'54512',9),
+(4,'98745',4)
+
+select * from Kitaplar
+insert into Kitaplar values
+('45266','anna karerina','1875',25),
+('124512','kara kedi','2656',41),
+('262646','tutunamayanlar','54969',57)
+
+select * from KitapYazar
+insert into KitapYazar values('66496',547),
+('661649',847),
+('554964',413)
+
+select * from Kutuphane
+insert into Kutuphane values(24,'talas','alýndý',10),
+(23,'melikgazi','alýnmadý',34),
+(41,'kocasinan','alýndý',23)
+
+select * from uyeler
+insert into UYELER values(
+
+select * from Yazarlar
+insert into Yazarlar values(2,'sabahattin'),
+(4,'kafka'),
+(9,'zweig')
+
+select * from KitapKutuphane
+insert into KitapKutuphane values(24,'454558',78),
+(45,'65414',35),
+(21,'12455',28)
+
+
+
+
+
